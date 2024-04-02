8dd31db8d063945406f80825cacfb8b952716443
Aaron Tavistock <aaron@geekshow.com>
2024-04-02 08:43:56 -0700

    Some reformatting

--- lib/types/us_phone_number.ex
+++ lib/types/us_phone_number.ex
@@ -2,12 +2,12 @@ defmodule EctoTypes.Types.UsPhoneNumber do
   @moduledoc """
     Behaviors for normalizing, storing, and retrieving US phone numbers.
 
-    Any string which can be reduced to an 11 digit number with a "1" prefix or a 10 digit
-    number, is considered a valid US format number.  This can be stored in the
-    database as a :bigint field rather than a string.  When retrieved from the database it
-    will be reformated into a string in a standard US format as "1(nnn)nnn-nnnn".
+    Any string which can be reduced to an 11 digit number with a "1" prefix or a ten
+    digit number, is considered a valid US format number.  This can be stored in the
+    database as a :bigint field rather than a string.  When retrieved from the database
+    it will be reformated into a string in a standard US format as `1 (nnn) nnn-nnnn`.
 
-    There is no validation for actual real world phone numbers, only that they are in the
+    There is no validation for real world phone numbers, only that they are in the
     correct format.
   """
 
@@ -22,62 +22,123 @@ defmodule EctoTypes.Types.UsPhoneNumber do
             when is_integer(value) and value > 1_999_999_999 and value < 9_999_999_999
 
   @impl Ecto.Type
-  def type, do: :int8
+  def type do
+    :int8
+  end
 
   @impl Ecto.Type
   @spec cast(any) :: :error | {:ok, binary}
-  def cast(phone) when is_binary(phone), do: {:ok, phone}
-  def cast(phone) when is_integer(phone), do: load(phone)
-  def cast(_), do: :error
+  def cast(value) when is_binary(value) do
+    {:ok, value}
+  end
+
+  def cast(value) when is_integer(value) do
+    load(value)
+  end
+
+  def cast(_) do
+    :error
+  end
 
   @impl Ecto.Type
   @spec load(any) :: :error | {:ok, binary}
-  def load(value) when ten_digit_numeric(value), do: {:ok, load!(value)}
-  def load(number) when integer_phone_bounds(number), do: load(Integer.to_string(number))
-  def load(_), do: :error
+  def load(value) when ten_digit_numeric(value) do
+    {:ok, load!(value)}
+  end
 
-  @spec load!(any) :: :error | binary
-  def load!(value) when ten_digit_numeric(value), do: String.replace(value, @split, @format)
-  def load!(number) when integer_phone_bounds(number), do: load!(Integer.to_string(number))
-  def load!(_), do: :error
+  def load(number) when integer_phone_bounds(number) do
+    number |> Integer.to_string() |> load()
+  end
+
+  def load(_) do
+    :error
+  end
 
   @impl Ecto.Type
   @spec dump(any) :: :error | {:ok, pos_integer}
-  def dump(number) when integer_phone_bounds(number), do: {:ok, number}
-  def dump(phone) when is_binary(phone), do: phone |> pure_numeric |> numeralize |> dump
-  def dump(_), do: :error
+  def dump(number) when integer_phone_bounds(number) do
+    {:ok, number}
+  end
+
+  def dump(value) when is_binary(value) do
+    value |> scrub_phone_string() |> dump()
+  end
+
+  def dump(_) do
+    :error
+  end
+
+  @spec load!(any) :: :error | binary
+  def load!(value) when ten_digit_numeric(value) do
+    value |> String.replace(@split, @format)
+  end
+
+  def load!(number) when integer_phone_bounds(number) do
+     number |> Integer.to_string() |> load!()
+  end
+
+  def load!(_) do
+     :error
+  end
 
   @spec dump!(any) :: :error | pos_integer
-  def dump!(number) when integer_phone_bounds(number), do: number
-  def dump!(phone) when is_binary(phone), do: phone |> pure_numeric |> numeralize |> dump!
-  def dump!(_), do: :error
+  def dump!(number) when integer_phone_bounds(number) do
+     number
+  end
+
+  def dump!(value) when is_binary(value) do
+     value |> scrub_phone_string() |> dump!()
+  end
+
+  def dump!(_) do
+     :error
+  end
 
   # TODO Move many of these formatting functions to a PhoneFormat module
   @spec valid?(any) :: boolean
-  def valid?(number) when integer_phone_bounds(number), do: true
-  def valid?(phone) when is_binary(phone), do: phone |> pure_numeric |> numeralize |> valid?
-  def valid?(_), do: false
+  def valid?(number) when integer_phone_bounds(number) do
+     true
+  end
+
+  def valid?(value) when is_binary(value) do
+     value |> scrub_phone_string() |> valid?()
+  end
+
+  def valid?(_) do
+     false
+  end
 
   @spec format(binary | non_neg_integer) :: binary
   def format(nil), do: ""
   def format(:error), do: ""
   def format(0), do: ""
-  def format(number) when is_integer(number), do: Integer.to_string(number) |> format()
-  def format(phone) when ten_digit_numeric(phone), do: String.replace(phone, @split, @format)
-  def format(other) when is_binary(other), do: other
 
-  @spec reformat(binary) :: binary
-  def reformat(phone), do: phone |> dump!() |> format()
+  def format(number) when is_integer(number) do
+     number |> Integer.to_string() |> format()
+  end
 
-  @spec strip_one_prefix(binary) :: binary
-  def strip_one_prefix("1" <> value), do: value
-  def strip_one_prefix(value), do: value
+  def format(value) when ten_digit_numeric(value) do
+     value |> String.replace(@split, @format)
+  end
 
-  # Remove any non-numeric letters from a string
-  defp pure_numeric(phone), do: phone |> String.replace(~r/\D/, "")
+  def format(value) when is_binary(value) do
+     value |> dump!() |> format()
+  end
 
   # Convert a ten digit numeric string into an integer or return :error
-  defp numeralize(value) when ten_digit_numeric(value), do: String.to_integer(value)
-  defp numeralize("1" <> value) when is_binary(value), do: numeralize(value)
-  defp numeralize(_), do: :error
+  defp scrub_phone_string(value) do
+    value |> String.replace(~r/\D/, "") |> scrub_phone_string()
+  end
+
+  defp scrub_phone_string("1" <> value) when ten_digit_numeric(value) do
+    value |> String.to_integer()
+  end
+
+  defp scrub_phone_string(value) when ten_digit_numeric(value) do
+     value |> String.to_integer()
+  end
+
+  defp scrub_phone_string(_) do
+     :error
+  end
 end
